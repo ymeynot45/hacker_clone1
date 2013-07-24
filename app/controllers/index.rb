0c36e85063cd7f9f822b82808493b82ccdff4a2d
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-24 09:52:04 -0500

    added login page and some functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,38 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/login' do
+
+  erb :login
+end
+
+get '/profile/:user_id' do
+
+  erb :profile
+end
+
+get '/post/:post_id' do
+
+  erb :post
+end
+
+post '/login' do
+
+  redirect "/profile/#{user.id}"
+end
+
+post '/register' do
+  user = User.new(params[:user])
+  if user.valid?
+    user.save
+    session[:user_id] = user.id
+  redirect "/profile/#{user.id}"
+  else
+    #@errors
+    redirect "/login"
+  end
+end
+
+
+
